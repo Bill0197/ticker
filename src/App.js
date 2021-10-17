1a9ce5f8d71ff9e13b6232aab681af3f045be9bb
Khabibullo Saydullaev <khabibullosaydullaev@gmail.com>
2021-10-17 17:59:03 +0500

    made the App component functional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,62 +13,57 @@ import PortfolioContainer from './containers/PortfolioContainer';
 import NavBarContainer from './containers/NavBarContainer';
 import SelectAction from './components/SelectAction';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className='App container-fluid'>
-          <div className='row'>
-            <div className='col-12 '>
-              <NavBarContainer color='success' />
+const App = () => {
+  return (
+    <Router>
+      <div className='App container-fluid'>
+        <div className='row'>
+          <div className='col-12 '>
+            <NavBarContainer color='success' />
+          </div>
+        </div>
+        <div className='row my-3 full-height'>
+          <div className='col-xl-4 col-lg-12'>
+            <div className='col-12 show-border'>
+              <StocksTableContainer />
             </div>
           </div>
-          <div className='row my-3 full-height'>
-            <div className='col-xl-4 col-lg-12'>
-              <div className='col-12 show-border'>
-                <StocksTableContainer />
-              </div>
-            </div>
-            <div className='col-xl-8 col-lg-12'>
-              <div className='col-12'>
-                <div className='row show-border'>
-                  <div className='col'>
-                    <DateSliderContainer />
-                  </div>
+          <div className='col-xl-8 col-lg-12'>
+            <div className='col-12'>
+              <div className='row show-border'>
+                <div className='col'>
+                  <DateSliderContainer />
                 </div>
-                <div className='row show-border my-3'>
-                  <div className='col-12 full-height'>
-                    <div>
-                      <SelectAction />
-                      <Switch>
-                        <Route
-                          exact
-                          path='/'
-                          render={() => <Redirect to='/transactions' />}
-                        />
-                        <Route
-                          path='/trade/:ticker?'
-                          component={TradeContainer}
-                        />
-                        <Route
-                          path='/transactions'
-                          component={TransactionsContainer}
-                        />
-                        <Route
-                          path='/portfolio'
-                          component={PortfolioContainer}
-                        />
-                      </Switch>
-                    </div>
+              </div>
+              <div className='row show-border my-3'>
+                <div className='col-12 full-height'>
+                  <div>
+                    <SelectAction />
+                    <Switch>
+                      <Route
+                        exact
+                        path='/'
+                        render={() => <Redirect to='/transactions' />}
+                      />
+                      <Route
+                        path='/trade/:ticker?'
+                        component={TradeContainer}
+                      />
+                      <Route
+                        path='/transactions'
+                        component={TransactionsContainer}
+                      />
+                      <Route path='/portfolio' component={PortfolioContainer} />
+                    </Switch>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </Router>
-    );
-  }
-}
+      </div>
+    </Router>
+  );
+};
 
 export default App;
